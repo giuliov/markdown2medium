bb347963aa52e9aeed9193b442ef69d41d7fb1f2
Giulio Vian <giuliovdev@hotmail.com>
2020-05-30 18:06:10 +0100

    support for images
    this time using goldmark extension mechanism

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # markdown2medium
 Inspired by cjoudrey/md2medium, I want a simple way to re-publish in Medium some Markdown posts written for Hugo.
+
+## Usage
+
+```
+markdown2medium                                                                                                     \
+    --mediumIntegrationToken 1234567890abcdef1234567890abcdef123457890abcdef123456789abcdef1234                     \
+    --markdownFile C:/myblog/content/post/2019/pyp/1-hosts/index.md                                                 \
+    --canonicalURL http://blog.casavian.eu/2019/08/23/meta-pipelines-part-1-docker-hosts/                           \
+    --publishStatus draft                                                                                           \
+    --originalNote 'Originally posted at [{{ .BaseURL }}]({{ .CanonicalURL }}) on {{ .Date.Format \"2 Jan 2006\" }}'\
+    --dryRun                                                                                                        \
+    --debug
+```
--- src/main.go
+++ src/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"mdext"
+	"mime"
 	"net/url"
 	"os"
+	"path"
 	"text/template"
 	"time"
 
@@ -15,21 +18,26 @@ import (
 	"github.com/jessevdk/go-flags"
 	"github.com/yuin/goldmark"
 	"gopkg.in/yaml.v2"
+	// highlighting "github.com/yuin/goldmark-highlighting"
 )
 
 // Options receive command line parameters
 type Options struct {
 	MediumAccessToken string `short:"t" long:"mediumIntegrationToken" description:"Medium Access Token" required:"true" json:"medium_access_token"`
-	//	MediumUser        string `short:"u" long:"mediumUserName" description:"Medium Username" required:"false" json:"medium_user_id"`
-	CanonicalURL  string `short:"c" long:"canonicalURL" description:"URL of original post" required:"false" `
-	FilePath      string `short:"i" long:"markdownFile" description:"Path to Markdown file to post on Medium" required:"true"`
-	PublishStatus string `short:"s" long:"publishStatus" description:"Status of the post: public, draft (default), or unlisted" required:"false" default:"draft" json:"medium_publish_status"`
-	OriginalNote  string `short:"p" long:"originalNote" description:"Paragraph to append pointing to original post" required:"false"`
+	CanonicalURL      string `short:"c" long:"canonicalURL" description:"URL of original post" required:"false" `
+	FilePath          string `short:"i" short:"f" long:"markdownFile" description:"Path to Markdown file to post on Medium" required:"true"`
+	PublishStatus     string `short:"s" long:"publishStatus" description:"Status of the post: public, draft (default), or unlisted" required:"false" default:"draft" json:"medium_publish_status"`
+	OriginalNote      string `long:"originalNote" description:"Paragraph to append pointing to original post" required:"false"`
+	DryRun            bool   `long:"dryRun" description:"Run through the Markdown data but does not upload to Medium" required:"false"`
+	Debug             bool   `long:"debug" description:"Additional logging and HTML dump" required:"false"`
 }
 
 func main() {
+
 	var opts Options
-	flags.Parse(&opts)
+	if _, err := flags.Parse(&opts); err != nil {
+		log.Fatalf("Failed to parse arguments: %s", err)
+	}
 
 	PublishMarkdownFile2Medium(&opts)
 }
@@ -38,43 +46,68 @@ func main() {
 func PublishMarkdownFile2Medium(opts *Options) {
 	log.Println("Processing ", opts.FilePath)
 
-	metadata, content, err := ParseInputMarkdownFile(opts.FilePath)
+	metadata, content, err := ParseFrontMatter(opts.FilePath)
 	if err != nil {
 		log.Fatalf("Failed to parse Markdown file: %s", err)
 	}
 
 	buf := ComposeFinalMarkdown(content, opts, metadata)
 
-	html, err := MarkdownToHTML(buf)
+	mediumClient := medium.NewClientWithAccessToken(opts.MediumAccessToken)
+
+	mediumImageUploader := func(originPath string) (string, error) {
+		basePath := path.Dir(opts.FilePath)
+		imgPath := path.Join(basePath, originPath)
+		if opts.DryRun {
+			log.Printf("Should upload image file %s", imgPath)
+			return originPath, nil
+		} else {
+			log.Printf("Uploading image file %s", imgPath)
+			// upload file to Medium and get image URL back
+			image, err := mediumClient.UploadImage(medium.UploadOptions{
+				FilePath:    imgPath,
+				ContentType: mime.TypeByExtension(path.Ext(originPath)),
+			})
+			if err != nil {
+				return originPath, err
+			}
+			return image.URL, nil
+		}
+	}
+
+	html, err := MarkdownToHTML(buf, mediumImageUploader)
 	if err != nil {
 		log.Fatalf("Failed to convert Markdown to HTML: %s", err)
 	}
 
-	mediumClient := medium.NewClientWithAccessToken(opts.MediumAccessToken)
+	if opts.Debug {
+		f := path.Base(opts.FilePath) + ".html"
+		ioutil.WriteFile(f, []byte(html), 0644)
+	}
+
 	user, err := mediumClient.GetUser("")
 	if err != nil {
 		log.Fatalf("Failed to load Medium user: %s", err)
 	}
 	publishStatus := medium.PublishStatus(opts.PublishStatus)
 
-	// TODO support for Images !!!!!!!!!!
-	// if err := htmlFormatter.ReplaceImages(mediumClient); err != nil {
-	// 	logger.Error(fmt.Sprintf("Error replacing images: %s", err))
-	// }
-
-	post, err := mediumClient.CreatePost(medium.CreatePostOptions{
-		UserID:        user.ID,
-		Title:         metadata.Title,
-		Content:       html,
-		ContentFormat: medium.ContentFormatHTML,
-		Tags:          metadata.Tags,
-		PublishStatus: publishStatus,
-		CanonicalURL:  opts.CanonicalURL,
-	})
-	if err != nil {
-		log.Fatal(err)
+	if opts.DryRun {
+		fmt.Printf("Post %s not published (dry run)\n", opts.FilePath)
+	} else {
+		post, err := mediumClient.CreatePost(medium.CreatePostOptions{
+			UserID:        user.ID,
+			Title:         metadata.Title,
+			Content:       html,
+			ContentFormat: medium.ContentFormatHTML,
+			Tags:          metadata.Tags,
+			PublishStatus: publishStatus,
+			CanonicalURL:  opts.CanonicalURL,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("New %s post published at %s\n", post.PublishState, post.URL)
 	}
-	fmt.Printf("New %s post published at %s\n", post.PublishState, post.URL)
 }
 
 // FrontMatterMetadata receives minimal Front Matter metadata
@@ -84,8 +117,8 @@ type FrontMatterMetadata struct {
 	Tags  []string  `yaml:"tags"`
 }
 
-// ParseInputMarkdownFile read the Markdown file, extracting any YAML or TOML front matter
-func ParseInputMarkdownFile(filePath string) (*FrontMatterMetadata, []byte, error) {
+// ParseFrontMatter read the Markdown file, extracting any YAML or TOML front matter
+func ParseFrontMatter(filePath string) (*FrontMatterMetadata, []byte, error) {
 
 	var metadata FrontMatterMetadata
 	var content []byte
@@ -166,6 +199,7 @@ func ComposeFinalMarkdown(content []byte, opts *Options, metadata *FrontMatterMe
 		log.Fatalf("Failed to parse Canonical URL: %s", err)
 	}
 	context := TemplateContext{
+		// fields available in template
 		BaseURL:      fmt.Sprintf("%s//%s", u.Scheme, u.Host),
 		CanonicalURL: opts.CanonicalURL,
 		Title:        metadata.Title,
@@ -183,10 +217,23 @@ func ComposeFinalMarkdown(content []byte, opts *Options, metadata *FrontMatterMe
 }
 
 // MarkdownToHTML converts Markdown to HTML using goldmark
-func MarkdownToHTML(markdown *bytes.Buffer) (string, error) {
-	var buf bytes.Buffer
+func MarkdownToHTML(source *bytes.Buffer, imagePathHandler mdext.ImagePathHandler) (string, error) {
 
-	err := goldmark.Convert(markdown.Bytes(), &buf)
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			// TODO Medium hates this?
+			// // TODO expose some options
+			// highlighting.NewHighlighting(
+			// 	highlighting.WithStyle("monokai"),
+			// 	highlighting.WithFormatOptions(
+			// 		html.WithLineNumbers(true),
+			// 	),
+			// ),
+			mdext.NewImageExt(imagePathHandler),
+		),
+	)
+	var buf bytes.Buffer
+	err := markdown.Convert(source.Bytes(), &buf)
 
 	return buf.String(), err
 }
